cd3e38a55d4977c7e7839b901c523fd3fa218d63
Jorge Mager <Jorge.Mager@cellebrite.com>
2023-03-28 15:10:42 +0300

    Setting up our React app

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>YesOrNo</h1>
+        <p className="text-muted">A simple yes or no question app</p>
+      </div>
+    </div>
+  );
+}
